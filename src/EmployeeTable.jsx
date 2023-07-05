6ea82e7c083b58c5fa6aaea632d35ae5481aad5f
Talha <ka889076@gmail.com>
2023-07-05 16:59:31 +0500

    added 1 button

---
+++ src/EmployeeTable.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const EmployeeTable = () => {
+  return (
+    <div>
+        <button id='selfie_btn'>Take selfie</button>
+    </div>
+  )
+}
+
+export default EmployeeTable;
